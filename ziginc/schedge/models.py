71e4c85bb1421a58aff52bbf351bd85d2e4cbd75
alexandertorkelsen <alexander.torkelsen@gmail.com>
2021-02-05 13:47:55 +0100

    Added dummy Event model to be used in the implementation of mypage.

--- ziginc/schedge/models.py
+++ ziginc/schedge/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    titel = models.CharField(max_length=100)
+    startDate = models.DateField()
+    enddate = models.DateField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    duration = models.DurationField()
+    description = models.CharField(max_length=500)
+    location = models.CharField(max_length=100)
+    # hostID = models.ForeignKey()
+    # groupID = models.ForeignKey()
