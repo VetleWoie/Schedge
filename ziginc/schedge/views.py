422eae36228b3197773966b52078a4db6b43efa8
alexandertorkelsen <alexander.torkelsen@gmail.com>
2021-02-05 14:50:01 +0100

    Added view to mypage, that uses render to generate a mypage with all the events

--- ziginc/schedge/views.py
+++ ziginc/schedge/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Event
 
 # Create your views here.
+
+def mypage(request):
+    events = Event.object.all()
+    context = {'events':events}
+    return render(request, 'mypage.html', context)
