ce3306752aa821a6cb8db0099a5cabdb81f0de7d
alexandertorkelsen <alexander.torkelsen@gmail.com>
2021-02-08 17:02:46 +0100

    Removed generator and deletion of dummy events. Realized superuser can be used for this

--- ziginc/schedge/views.py
+++ ziginc/schedge/views.py
@@ -4,23 +4,8 @@ from .models import Event
 import datetime #Only used to get som eevents into the dummy database. Remove before deployment.
 
 # Create your views here.
-num_dummy_events = 4
-
-def createrandomevents():
-    for i in range(num_dummy_events):
-        Event.objects.create(title=f'BestEventEver{i}', 
-                            duration = "14",
-                            description='This is really the best event ever.',
-                            location = f"YOmama{i}")
-def deleterandomevents():
-    for i in range(num_dummy_events):
-        Event.objects.filter(title=f'BestEventEver{i}').delete()
-
 
 def mypage(request):
-    deleterandomevents()
-    createrandomevents()
-    # Event.objects.all().delete()
     events = Event.objects.all()
     context = {'events':events}
     return render(request, 'mypage.html', context)
