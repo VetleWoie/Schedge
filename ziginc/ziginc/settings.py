49257aaf5e0ff3cfcbfc64c4bc6743b439cad887
eliasriise <eliasriise@outlook.com>
2021-02-16 16:29:31 +0100

    made base, sign in/out/up

--- ziginc/ziginc/settings.py
+++ ziginc/ziginc/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
