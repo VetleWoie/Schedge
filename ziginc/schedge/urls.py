24c709b7159fe8b47155071e10310a1a0518eca5
Vetle <vho023@uit.no>
2021-02-16 16:30:02 +0100

    password reset works, but cant find the templates

--- ziginc/schedge/urls.py
+++ ziginc/schedge/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic.base import TemplateView
 from . import views
 
@@ -7,4 +7,5 @@ from .views import SignUpView
 urlpatterns = [
     path('signup/' ,SignUpView.as_view(),name='signup'),    
     path("createEvent", views.createEvent, name="createEvent"),
+    path('/', include('django.contrib.auth.urls')),
 ]
