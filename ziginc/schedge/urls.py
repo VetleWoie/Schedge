7fd8c1b08a4d7ade607b7587dbe17f7cf7cc48fb
Vetle <vho023@uit.no>
2021-02-16 13:50:22 +0100

    sign in page

--- ziginc/schedge/urls.py
+++ ziginc/schedge/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
 
+from .views import SignUpView
+
 urlpatterns = [
-    # path("", TemplateView.as_view(template_name='home.html'), name="home"),
-    path("signUp", views.signUp, name="signUp"),
-    path("signIn", views.signIn, name="signIn"),
+    path('signup/' ,SignUpView.as_view(),name='signup'),    
     path("createEvent", views.createEvent, name="createEvent"),
 ]
--- ziginc/schedge/views.py
+++ ziginc/schedge/views.py
@@ -1,23 +1,17 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 # from .models import User
 # Create your views here.
-# def home(request):
-#     # users = User.objects.order_by("username")
-#     template = loader.get_template("home.html")
-#     context = {
-#         "userList" : ""
-#     }
-#     return HttpResponse(template.render(context, request))
-    # raise Http404("THIS DOES NOT Existss") #tis' was but a test
-def signUp(request):
-    return HttpResponse("signup page")
-def signIn(request):
-    template = loader.get_template("registration/login.html")
-    context = {
-        "":"" 
-    }
-    return HttpResponse(template.render(context, request))
+
 def createEvent(request):
     raise Http404("THIS DOES NOT Existss") #tis' was but a test
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
