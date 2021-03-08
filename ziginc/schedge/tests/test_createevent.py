802c00b8df8ed8c74c4d9f819610fb513a04c533
Theodor Tollersrud <theo.tollers@gmail.com>
2021-03-08 13:41:22 +0100

    fixed tests and issue #1

--- ziginc/schedge/tests/test_createevent.py
+++ ziginc/schedge/tests/test_createevent.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from schedge.models import Event, TimeSlot
 from schedge.forms import EventForm
+from django.contrib.auth.models import User
 import datetime as dt
 from django.http import JsonResponse
 
-class CourseModelTest(TestCase):
+class EventModelTest(TestCase):
     def setUp(self):
         self.example_form = {
             "title": "hiking",
@@ -16,11 +17,16 @@ class CourseModelTest(TestCase):
             "enddate":"2025-03-03",
             "duration": "00:10:00"
         }
+        user = User.objects.create_user('tester', 'myemail@test.com', 'Elias123')
+
+        self.client.login(username='tester', password='Elias123')
+
 
     def test_create(self):
         response = self.client.post("/createevent/", self.example_form)
 
         self.assertEqual(response.status_code, 302)  # redirects
+        print("urlen\n\n\n" + response.url, "\n\n\n")
         self.assertRegex(response.url, "^\/event\/\d+\/$") # url is /event/[some number]/
         
         id_ = "".join(c for c in response.url if c.isnumeric()) # the number in the url
@@ -57,6 +63,7 @@ class CourseModelTest(TestCase):
         invalid_form["startdate"] = "1969-07-20"
 
         response = self.client.post("/createevent/", invalid_form)
+        print("\n\n" + response.url + "\n\n")
         self.assertEqual(response.status_code, 400)
         getresponse = self.client.get("/event/1/")
         # print(getresponse.context)
