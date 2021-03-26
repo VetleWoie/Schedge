1261376cdf59151c06784c83ddd165e0dad3e8a7
Iris <iam014@uit.no>
2021-03-26 15:33:58 +0100

    Created tests for homepage

--- ziginc/schedge/tests/__init__.py
+++ ziginc/schedge/tests/__init__.py
@@ -7,9 +7,11 @@ from .functional.test_eventedit import *
 from .functional.test_eventpage import *
 from .functional.test_timeslots import *
 from .functional.test_timeSlot import *
+from .functional.test_homepage import *
 from .functional.test_invite import *
 from .functional.test_mypage import *
 from .functional.test_signup import *
 from .functional.test_login import *
 from .functional.tests import *
+
 from .modeltests.test_timeslots import *
