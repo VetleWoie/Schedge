035df28429fe7c42f18f7c7d217da6e4c3fe23b3
VetleWoie <vet.h.woie@hotmail.com>
2021-03-26 01:51:26 +0100

    Fixed name change of potential timeslot test file

--- ziginc/schedge/tests/__init__.py
+++ ziginc/schedge/tests/__init__.py
@@ -1,12 +1,12 @@
 from .scenario.test_createevent_selenium import *
 
+from .functional.test_potentialTimeSlot import *
 from .functional.test_notifications import * 
 from .functional.test_participant import *
 from .functional.test_createevent import *
 from .functional.test_eventedit import *
 from .functional.test_eventpage import *
 from .functional.test_timeslots import *
-from .functional.test_timeSlot import *
 from .functional.test_invite import *
 from .functional.test_mypage import *
 from .functional.test_signup import *
