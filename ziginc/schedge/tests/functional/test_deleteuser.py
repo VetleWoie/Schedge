91638b47aaa90547fc73a7ddd9c8412542e818cf
ato058 <ato058@uit.no>
2021-04-27 17:00:49 +0200

    Test if user creats event and is then deleted, the event is also removed

--- ziginc/schedge/tests/functional/test_deleteuser.py
+++ ziginc/schedge/tests/functional/test_deleteuser.py
@@ -3,10 +3,15 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from schedge.forms import NameForm
 from unittest import skip
+import datetime as dt
+from django.utils.timezone import now
+from schedge.models import Event
 
 class DeleteUserTest(TestCase):
     def setUp(self):
-        # self.goodUser = request.post
+
+        self.host = User.objects.create_user("host", "host@test.com", "Elias123")
+
         self.userGood = {
             "username" : "test",
             "first_name" : "Test",
@@ -15,8 +20,18 @@ class DeleteUserTest(TestCase):
             "password" : "Elias123",
             "password2" : "Elias123",
         }
+        self.other = {
+            "username" : "test1",
+            "first_name" : "Test1",
+            "last_name" : "ing1",
+            "email" : "test1@ing.com",
+            "password" : "Elias123",
+            "password2" : "Elias123",
+        }
 
-    def test_deluser(self):
+        
+
+    def test_delete_user(self):
         response = self.client.post("/signup/", self.userGood)
         self.assertEqual(response.status_code, 302)
 
@@ -29,7 +44,7 @@ class DeleteUserTest(TestCase):
         self.assertEqual(usr_exists, False)
 
     @skip("Need the home view which is not present in this version of master, but which is implemented in another branch.")
-    def test_deluser_redirect(self):
+    def test_delete_user_redirect(self):
         response = self.client.post("/signup/", self.userGood)
         self.assertEqual(response.status_code, 302)
 
@@ -37,3 +52,43 @@ class DeleteUserTest(TestCase):
 
         response2 = self.client.post("/mypage/delete_user_account/")
         self.assertEqual(response2.url, "/home/")
+
+    # TODO: Create event -> delete user -> see that event is also deleted.
+    def test_delete_event_host(self):
+        # CREATE USER
+        response = self.client.post("/signup/", self.userGood)
+        self.assertEqual(response.status_code, 302)
+        
+        usr = User.objects.get(username=self.userGood['username'])
+        # LOGIN USER
+        self.client.login(username=self.userGood["username"], password=self.userGood["password"])
+
+        # CREATE EVENT
+        self.example_model = {
+            "title": "date night",
+            "location": "the lightning route",
+            "description": ";)",
+            "starttime": dt.time(),
+            "endtime": dt.time(hour=14),
+            "startdate": now(),
+            "enddate": now() + dt.timedelta(days=1),
+            "duration": dt.timedelta(hours=2),
+            "host": usr,
+        }
+        self.date = Event.objects.create(**self.example_model)
+
+        self.date.participants.add(usr)
+
+        # DELETE USER/HOST
+        self.client.post("/mypage/delete_user_account/")
+
+        del_event = Event.objects.filter(host=usr).exists()
+
+        self.assertEqual(del_event, False)
+
+
+
+
+
+    # TODO: Create timeslot -> delete user -> see that timeslot is removed.
+    # TODO: Create event -> invite guest -> delete user -> check if participant has access to event(shoulnd't).
