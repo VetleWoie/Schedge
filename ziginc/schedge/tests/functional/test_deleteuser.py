4f171d8f92596780edba734f25aef780aebdadf7
ato058 <ato058@uit.no>
2021-04-27 17:27:33 +0200

    Checks if delted user (as host) results in event beeing deleted, and if a timeslot made by a deleted user is deleted. Both of wich seems to be true

--- ziginc/schedge/tests/functional/test_deleteuser.py
+++ ziginc/schedge/tests/functional/test_deleteuser.py
@@ -6,6 +6,8 @@ from unittest import skip
 import datetime as dt
 from django.utils.timezone import now
 from schedge.models import Event
+from schedge.utils import create_time_slot
+from schedge.models import Event, TimeSlot
 
 class DeleteUserTest(TestCase):
     def setUp(self):
@@ -53,7 +55,6 @@ class DeleteUserTest(TestCase):
         response2 = self.client.post("/mypage/delete_user_account/")
         self.assertEqual(response2.url, "/home/")
 
-    # TODO: Create event -> delete user -> see that event is also deleted.
     def test_delete_event_host(self):
         # CREATE USER
         response = self.client.post("/signup/", self.userGood)
@@ -75,6 +76,7 @@ class DeleteUserTest(TestCase):
             "duration": dt.timedelta(hours=2),
             "host": usr,
         }
+        
         self.date = Event.objects.create(**self.example_model)
 
         self.date.participants.add(usr)
@@ -82,13 +84,62 @@ class DeleteUserTest(TestCase):
         # DELETE USER/HOST
         self.client.post("/mypage/delete_user_account/")
 
-        del_event = Event.objects.filter(host=usr).exists()
+        del_event = Event.objects.filter(id=self.date.id).exists()
 
         self.assertEqual(del_event, False)
 
+    def test_delete_event_host_with_timeslot(self):
+        # CREATE USER
+        response = self.client.post("/signup/", self.userGood)
+        self.assertEqual(response.status_code, 302)
+        usr = User.objects.get(username=self.userGood['username'])
+        
+        # CREATE OTHER USER
+        response_other = self.client.post("/signup/", self.other)
+        self.assertEqual(response_other.status_code, 302)
+        other = User.objects.get(username=self.other['username'])
+        
+        # LOGIN USER
+        self.client.login(username=self.userGood["username"], password=self.userGood["password"])
 
+        # CREATE EVENT
+        self.example_model = {
+            "title": "date night",
+            "location": "the lightning route",
+            "description": ";)",
+            "starttime": dt.time(),
+            "endtime": dt.time(hour=14),
+            "startdate": now(),
+            "enddate": now() + dt.timedelta(days=1),
+            "duration": dt.timedelta(hours=2),
+            "host": usr,
+        }
+        self.date = Event.objects.create(**self.example_model)
 
+        self.date.participants.add(usr)
 
+        # INVITE OTHER USER
+        form = {"invitee": other.id}
+        response = self.client.post(f"/event/{self.date.id}/invite/", form)
+        self.assertEqual(response.status_code, 302)
 
-    # TODO: Create timeslot -> delete user -> see that timeslot is removed.
-    # TODO: Create event -> invite guest -> delete user -> check if participant has access to event(shoulnd't).
+        # CREATE TIMESLOT FOR 'OTHER'
+        t1 = {
+            "start_time": dt.time(8,00,00),
+            "end_time": dt.time(10,00,00),
+            "date": (dt.datetime.now() + dt.timedelta(1)).date()
+        }
+        
+        create_time_slot(self.date, other, t1)
+
+        n_timeslots = TimeSlot.objects.filter(event=self.date)
+        self.assertEqual(len(n_timeslots),1)
+
+        self.client.logout()
+        self.client.login(username=self.other['username'], password=self.other['password'])        
+        
+        # DELETE 'OTHER'
+        self.client.post("/mypage/delete_user_account/")
+
+        n_timeslots = TimeSlot.objects.filter(event=self.date)
+        self.assertEqual(len(n_timeslots),0)        
