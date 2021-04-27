08bcf307f3ed4de32108ff004b9e4bdf9ca8edb2
ato058 <ato058@uit.no>
2021-04-27 14:57:41 +0200

    Added comment, and skips redirect test as home view is not present as of yet. It will be when another branch 'frontend' is merged with master.

--- ziginc/schedge/tests/functional/test_deleteuser.py
+++ ziginc/schedge/tests/functional/test_deleteuser.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from schedge.forms import NameForm
+from unittest import skip
 
 class DeleteUserTest(TestCase):
     def setUp(self):
@@ -23,10 +24,11 @@ class DeleteUserTest(TestCase):
 
         self.client.post("/mypage/delete_user_account/")
 
+        # Check that user does not exists after deletion.
         usr_exists = User.objects.filter(username=self.userGood["username"]).exists()
         self.assertEqual(usr_exists, False)
 
-
+    @skip("Need the home view which is not present in this version of master, but which is implemented in another branch.")
     def test_deluser_redirect(self):
         response = self.client.post("/signup/", self.userGood)
         self.assertEqual(response.status_code, 302)
@@ -34,4 +36,4 @@ class DeleteUserTest(TestCase):
         self.client.login(username=self.userGood["username"], password=self.userGood["password"])
 
         response2 = self.client.post("/mypage/delete_user_account/")
-        self.assertEqual(response2.url, "/signup/")
+        self.assertEqual(response2.url, "/home/")
