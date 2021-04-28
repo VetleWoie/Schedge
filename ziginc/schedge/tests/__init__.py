545c5089389bff22e269dd2e6f79e686311ee713
Iris Helen <iam014@uit.no>
2021-04-28 13:58:27 +0000

    Update ziginc/schedge/tests/__init__.py

--- ziginc/schedge/tests/__init__.py
+++ ziginc/schedge/tests/__init__.py
@@ -4,6 +4,8 @@ from .functional.test_potentialTimeSlot import *
 from .functional.test_notifications import * 
 from .functional.test_participant import *
 from .functional.test_createevent import *
+from .functional.test_deleteuser import *
+from .modeltests.test_timeslots import *
 from .functional.test_eventedit import *
 from .functional.test_eventpage import *
 from .functional.test_timeslots import *
@@ -12,5 +14,3 @@ from .functional.test_mypage import *
 from .functional.test_signup import *
 from .functional.test_login import *
 from .functional.tests import *
-from .functional.test_deleteuser import *
-from .modeltests.test_timeslots import *
