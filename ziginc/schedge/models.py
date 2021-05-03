1be8bb440e09f453c731724b35ea28069aa83ba7
eliasriise <eliasriise@outlook.com>
2021-05-03 17:32:23 +0200

    fix models

--- ziginc/schedge/models.py
+++ ziginc/schedge/models.py
@@ -13,7 +13,7 @@ from .utils import time_add, time_diff
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    friends = models.ManyToManyField('self', blank=True)
+    friends = models.ManyToManyField(User, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
