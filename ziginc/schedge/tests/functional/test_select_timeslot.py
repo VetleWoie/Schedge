cbc044d5f3dbac211b234f538bee2cc6cd50a427
ato058 <ato058@uit.no>
2021-05-07 15:30:27 +0200

    Added more coverage to test more of the test/functionality in the different views

--- ziginc/schedge/tests/functional/test_select_timeslot.py
+++ ziginc/schedge/tests/functional/test_select_timeslot.py
@@ -39,7 +39,29 @@ class SelectTimeslotTest(TestCase):
         self.guest = User.objects.create_user("guest", "guest@test.com", PASSWORD)
         self.date.participants.add(self.guest)
 
-    def test_gets_notification_on_time_selected(self):
+    def test_select_timeslot(self):
+        # create timeslot
+        form = {"start_time": "06:00:00", "end_time": "20:00:00", "date":self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.guest.username, password=PASSWORD)
+
+        form = {"start_time": "07:00:00", "end_time": "22:00:00", "date": self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.host.username, password=PASSWORD)
+
+        self.assertEqual(2, TimeSlot.objects.all().count())
+    
+        form = {"options": "10:00,12:00,{}".format(self.tomorrow)}
+        response = self.client.post(f"/event/{self.date.id}/select/", form)
+
+        response = self.client.get(f"event/{self.date.id}/")
+        date = Event.objects.get(id=self.date.id)
+        self.assertTrue(date.chosen_time)
+        # self.assertNotIn(response.context, "pts")
+    
+    def test_time_selected_wrong_method(self):
         """ Test notification when the host decides the time for an event"""
 
         # create timeslot
@@ -56,7 +78,47 @@ class SelectTimeslotTest(TestCase):
         self.assertEqual(2, TimeSlot.objects.all().count())
     
         form = {"options": "10:00,20:00,{}".format(self.tomorrow)}
+        response = self.client.get(f"/event/{self.date.id}/select/", form)
+        self.assertEqual(response.content, b"Request method not allowed")
+
+
+    def test_selected_timeslot_unequal_to_duration(self):
+        # create timeslot
+        form = {"start_time": "06:00:00", "end_time": "20:00:00", "date":self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.guest.username, password=PASSWORD)
+
+        form = {"start_time": "07:00:00", "end_time": "22:00:00", "date": self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.host.username, password=PASSWORD)
+
+        self.assertEqual(2, TimeSlot.objects.all().count())
+    
+        form = {"options": "10:00,11:00,{}".format(self.tomorrow)}
         response = self.client.post(f"/event/{self.date.id}/select/", form)
 
-        response = self.client.get(f"event/{self.date.id}/")
+        self.assertEqual(response.content, b"the selected time does not have the same length as the duration of the event")
         # self.assertNotIn(response.context, "pts")
+    
+    def test_time_selected_event_not_legal_event(self):
+        """ Test notification when the host decides the time for an event"""
+
+        # create timeslot
+        form = {"start_time": "06:00:00", "end_time": "20:00:00", "date":self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.guest.username, password=PASSWORD)
+
+        form = {"start_time": "07:00:00", "end_time": "22:00:00", "date": self.tomorrow}
+        self.client.post(f"/event/{self.date.id}/", form)
+
+        self.client.login(username=self.host.username, password=PASSWORD)
+
+        self.assertEqual(2, TimeSlot.objects.all().count())
+    
+        form = {"options": "10:00,20:00,{}".format(self.tomorrow)}
+        non_existent_event = 9999999
+        response = self.client.post(f"/event/{non_existent_event}/select/", form)
+        self.assertEqual(response.status_code, 404)
