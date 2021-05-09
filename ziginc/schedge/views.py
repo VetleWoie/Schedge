ea28062dbc53c58245b8421b855ef4f329c2c8a8
ato058 <ato058@uit.no>
2021-05-09 13:38:53 +0200

    If event does not exist raise Event.DoesNotExist instead of User.DoesNotExist

--- ziginc/schedge/views.py
+++ ziginc/schedge/views.py
@@ -467,7 +467,7 @@ def invite_delete(request, invite_id):
 def participant_delete(request, event_id, user_id):
     try:
         this_event = Event.objects.get(id=event_id)
-    except User.DoesNotExist:
+    except Event.DoesNotExist:
         return HttpResponseNotFound("Unknown event")
 
     if request.method != "POST":
